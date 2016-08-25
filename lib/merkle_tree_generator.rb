4c7bd5512b1ec218d898e8bf70a74b8b8e82eb5f
rakeshbs <rakeshbs@gmail.com>
2016-08-25 11:58:39 +0530

    Added Merkle Tree Generator

---
+++ lib/merkle_tree_generator.rb
@@ -0,0 +1,7 @@
+module ToyGit
+  class MerkleTreeGenerator
+    def generate(path:)
+      puts path
+    end
+  end
+end
---
+++ test/merkle_tree_generator_test.rb
@@ -0,0 +1,16 @@
+require_relative './test_helper.rb'
+require_relative '../lib/merkle_tree_generator.rb'
+
+module ToyGit
+  class TestMerkleTreeGenerator < Minitest::Test
+
+    def setup
+
+    end
+
+    def test_responds_to_generate
+      MerkleTreeGenerator.respond_to?(:generate)
+    end
+
+  end
+end
